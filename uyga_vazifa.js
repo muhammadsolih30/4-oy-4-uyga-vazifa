1ccb50e70944f6c46c794f085cfe2ff9e91e32d4
muhammadsolih <muhammadsolih08091011@gmail.com>
2025-08-10 19:43:35 +0500

    main

--- uyga_vazifa.js
+++ uyga_vazifa.js
@@ -1,16 +1,24 @@
 
+//! 1-masala
+
+// let n = 90;
+// let son = 65
+
+// if(n >= 65 && n <= 90 ){
+//    for (let i = 65; i <= n; i++) {
+//       console.log(son,":", String.fromCharCode(i))
+//       son++
+//    }
+// }
+// else{
+//    console.log(`siz 64 dan katta va 91 dan kichik bulgan son kirita olasiz`);
+// }
 
 
-let n = 90;
-let son = 65
 
-if(n >= 65 && n <= 90 ){
-   for (let i = 65; i <= n; i++) {
-      console.log(son,":", String.fromCharCode(i))
-      son++
-   }
-}
-else{
-   console.log(`siz 64 dan katta va 91 dan kichik bulgan son kirita olasiz`);
-}
+//! 2-masala
+
+
+
+
 
