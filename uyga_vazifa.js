76ca69ad09526e61bf547c1575dd7d4f03d1ccca
muhammadsolih <muhammadsolih08091011@gmail.com>
2025-08-12 12:35:18 +0500

    main

--- uyga_vazifa.js
+++ uyga_vazifa.js
@@ -18,6 +18,47 @@
 
 //! 2-masala
 
+// let n = `??:::`;
+
+// if(!isNaN(Number(n))){
+//    console.log("digit")
+// }else if ((n >= 'A' && n <= 'Z') || (n >= 'a' && n <= 'z')) {
+//       console.log('lotin'); 
+// }else{
+//    console.log('0');
+// }
+
+
+//! 3-masala
+
+// let sas = `hello ozbekistoa`
+
+// console.log(sas.charCodeAt(0));
+// console.log(sas.charCodeAt(sas.length -1))
+
+
+//! 4-masala
+
+
+
+// let n = 5;
+// let belgi = 'A ';
+// let a = ''
+
+
+// for (let i = 0; i < n; i++) {
+//    a += belgi
+// }
+// console.log(`${a}`);
+
+
+
+
+//! 5-masala
+
+
+
+
 
 
 
